ab096c2897a119099166f2c4170b7775f311edf7
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 06:06:54 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,6 +12,7 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     binding.pry
     @landmark = Landmark.create(params[:landmark])
+    @landmark.save
     redirect "/landmarks/#{@landmark.id}"
   end
 
