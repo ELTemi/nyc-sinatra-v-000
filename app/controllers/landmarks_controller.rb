2d0fa9ebfdfc853762856ff8a707d3b423f532ff
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 05:52:02 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,4 +9,12 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/new'
   end
 
+  post '/figures' do
+    @landmark = Landmark.create(name: params[:Name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params["genres"]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
 end
