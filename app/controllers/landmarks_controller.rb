bd27ae4fac5bd4b6f4d1e2805afd2c8d836b4e0d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 06:19:37 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -27,8 +27,6 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
-    binding.pry
-
     @landmark = Landmark.find_by_id(params[:id])
     @landmark.update(params[:landmark])
     @figure.save
