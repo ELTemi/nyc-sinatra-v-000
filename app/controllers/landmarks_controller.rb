a3342a93af0dcd44ebd44cd47e59518ce62a37f6
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 05:43:40 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,11 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/new' do
+    @titles = Title.all
+    @figures = Figures.all
+    erb :'/figures/new'
+  end
+
 end
