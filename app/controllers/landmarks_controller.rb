c146c153357e58401dadef7f342695717f16cf23
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 06:06:15 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,7 +10,6 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks' do
-    binding.pry
     @landmark = Landmark.create(params[:landmark])
     @figure = Figure.create(params)
     redirect "/landmarks/#{@landmark.id}"
